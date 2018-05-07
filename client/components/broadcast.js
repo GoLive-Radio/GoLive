1d6b0f7c782cc67bf8dc3def517ab1abc8c906e7
Nick Milazzo <nmils820@gmail.com>
2018-05-07 12:39:26 -0400

    Fixed auto-broadcast

--- client/components/broadcast.js
+++ client/components/broadcast.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 window.io = io;
 import RTCMultiConnection from 'rtcmulticonnection-v3';
 
 export class Broadcast extends Component {
   constructor(props) {
     super(props);
-    //bind
     this.startBroadcast = this.startBroadcast.bind(this);
   }
 
@@ -18,32 +17,29 @@ export class Broadcast extends Component {
     this.connection = new window.RTCMultiConnection();
     this.connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
-  //session setup
-  this.connection.session = {
-    audio: true,
-    video: false,
-    oneway: true
-  };
+    //session setup
+    this.connection.session = {
+      audio: true,
+      video: false,
+      oneway: true
+    };
 
-  //make sure I also don't see the video
-  this.connection.mediaConstraints.video = false;
+    //make sure I also don't see the video
+    this.connection.mediaConstraints.video = false;
 
-  //append it to the body
-  this.connection.onstream = (event) => {
-    document.body.appendChild(event.mediaElement);
-  }
+    //append it to the body
+    this.connection.onstream = event => {
+      document.body.appendChild(event.mediaElement);
+    };
 
-  const myID = this.props.match.params.broadcastId
-  return (
-    <div>
-      <h1>This is my test broadcast</h1>
-      {
-        myID ?
-        <button onClick={this.startBroadcast(myID)}>Broadcast me</button>
-        : null
-      }
-    </div>
+    const myID = this.props.match.params.broadcastId;
+    return (
+      <div>
+        <h1>This is my test broadcast</h1>
+        {myID ? (
+          <button onClick={e => this.startBroadcast(myID)}>Broadcast me</button>
+        ) : null}
+      </div>
     );
   }
 }
-
