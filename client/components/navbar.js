78f82bcdc63cc2a97e6ad8fcf711badf32f2946f
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-08 14:47:53 -0400

    removed comments

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -63,31 +63,3 @@ Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
 }
-
- 
-
-/*
-original menu 
-  <div>
-    <h1>Go Live</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in }
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in }
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-
-  */
