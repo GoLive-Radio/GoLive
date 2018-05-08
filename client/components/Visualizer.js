a014bf82580ff982f045af5be8dce0b3b6aa85c8
Alex Sobiloff <asobiloff@gmail.com>
2018-05-08 11:35:02 -0400

    style: reformat variable names

--- client/components/Visualizer.js
+++ client/components/Visualizer.js
@@ -21,10 +21,13 @@ export default class Visualizer extends Component{
     audio.controls = false;
     audio.loop = true;
     audio.autoplay = false;
+
     // Establish all variables that your Analyser will use
-    var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+    let canvas, ctx, source, context, analyser, fbcArray, bars, barX, barWidth, barHeight;
+
     // Initialize the MP3 player after the page loads all of its HTML into the window
     window.addEventListener('load', initMp3Player, false);
+
     function initMp3Player(){
       document.getElementById('audio_box').appendChild(audio);
       context = new AudioContext(); // AudioContext object instance
@@ -41,17 +44,17 @@ export default class Visualizer extends Component{
     // Looping at the default frame rate that the browser provides(approx. 60 FPS)
     function frameLooper(){
       window.requestAnimationFrame(frameLooper);
-      fbc_array = new Uint8Array(analyser.frequencyBinCount);
-      analyser.getByteFrequencyData(fbc_array);
+      fbcArray = new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(fbcArray);
       ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
       ctx.fillStyle = '#00CCFF'; // Color of the bars
       bars = 100;
       for (var i = 0; i < bars; i++) {
-        bar_x = i * 3;
-        bar_width = 2;
-        bar_height = -(fbc_array[i] / 2);
+        barX = i * 3;
+        barWidth = 2;
+        barHeight = -(fbcArray[i] / 2);
         //  fillRect( x, y, width, height ) // Explanation of the parameters below
-        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+        ctx.fillRect(barX, canvas.height, barWidth, barHeight);
       }
     }
   }
@@ -63,8 +66,8 @@ export default class Visualizer extends Component{
             <img onClick={this.toggleRecord} src={this.state.recording ? '/images/record_on.png' : '/images/record.png'} />
         </div>
         <div id="mp3_player">
-          <div id="audio_box"></div>
-          <canvas id="analyser_render"></canvas>
+          <div id="audio_box" />
+          <canvas id="analyser_render" />
         </div>
       </div>
     );
