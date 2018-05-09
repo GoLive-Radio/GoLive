f3cbd9dc5e52cf65dcbdbda120925e098af24506
rutaitp <rk2906@nyu.edu>
2018-05-09 18:24:36 -0400

    feat: finish edit My Account functionality

--- client/components/MyAccount.js
+++ client/components/MyAccount.js
@@ -1,26 +1,7 @@
 import React, { Fragment } from 'react';
 import {connect} from 'react-redux';
 import { Button, TextArea, Form } from 'semantic-ui-react';
-
-const MyAccount = ({user}) => {
-  return (
-    <Fragment>
-      <h1>My account</h1>
-      <Form>
-        <Form.Field>
-          <label>Profile picture</label>
-          <input placeholder={user.profilePic} />
-        </Form.Field>
-        <Form.Field>
-          <label>Tell us more about yourself</label>
-          <TextArea placeholder={user.summary} />
-        </Form.Field>
-        <Form.Field
-          control={Button} content="Save" label="Save" />
-      </Form>
-    </Fragment>
-  );
-};
+import { updateUserThunk } from '../store';
 
 /**
  * CONTAINER
@@ -31,5 +12,42 @@ const mapState = state => {
   };
 };
 
-export default connect(mapState)(MyAccount);
+const mapDispatchToProps = function(dispatch) {
+  return {
+    handleSubmit(evt, user) {
+      evt.preventDefault();
+      const action = updateUserThunk({
+        profilePic: evt.target.picture.value,
+        summary: evt.target.summary.value
+      }, user);
+      dispatch(action);
+    }
+  };
+};
+
+const MyAccount = (props) => {
+  return (
+    <Fragment>
+      <h1>My account</h1>
+      <Form onSubmit={(evt) => props.handleSubmit(evt, props.user)}>
+        <Form.Field>
+          <label>Profile picture</label>
+          <input
+            name="picture"
+            placeholder={props.user.profilePic} />
+        </Form.Field>
+        <Form.Field>
+          <label>Tell us more about yourself</label>
+          <TextArea
+            name="summary"
+            placeholder={props.user.summary} />
+        </Form.Field>
+        <Form.Field
+          control={Button} content="Save" label="Save" />
+      </Form>
+    </Fragment>
+  );
+};
+
+export default connect(mapState, mapDispatchToProps)(MyAccount);
 
