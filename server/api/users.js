5f9f7dea393b94628374d187a1e73c4537aad52e
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-09 15:19:11 -0400

    chore: added comments

--- server/api/users.js
+++ server/api/users.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
+// path '/users/:id'
 router.get('/:id', (req, res, next) => {
   User.findById(+req.params.id)
     .then(userById => res.json(userById))
