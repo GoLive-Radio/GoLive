1a2d97fdcafae760ad9d2154ca79fafa9262296f
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-09 11:52:01 -0400

    changed routes

--- server/api/stations.js
+++ server/api/stations.js
@@ -1,19 +1,35 @@
 const router = require('express').Router();
 const {Station} = require('../db/models');
+const { User } = require('../db/models')
+const { user_stations } = require('../db/models')
+
 module.exports = router;
 
 // exact path '/stations/
 router.get('/', (req, res, next) => {
-    Stations.findAll({})
+    Station.findAll({})
         .then(stations => res.json(stations))
         .catch(next);
 });
 
+// exact path '/stations/user/:id'
+router.get('/user/:id', (req, res, next) => {
+    Station.findAll({
+        include: [{
+            model: user_stations,
+            where: {
+                userId: +req.params.id
+            },
+            // required: true
+        }]
+    })
+    .then(stationsById => res.json(stationsById))
+    .catch(next)
+});
+
 // exact path '/stations/:id
 router.get('/:id', (req, res, next) => {
-    Stations.findById(+req.params.id)
+    Station.findById(+req.params.id)
         .then(station => res.json(station))
         .catch(next);
 });
-
-
