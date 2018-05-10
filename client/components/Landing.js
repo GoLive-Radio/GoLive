879700b3cf45c494a24d09372c76dbd5edb0e040
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-10 11:49:40 -0400

    chore: commit for new merge

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -13,12 +13,18 @@ export class Landing extends Component {
         return(
             <div className='main-container'>
                 <div className="main-body max-width max-height flex">
-                    <div className='auto-margin flex'>
+                    <div className='auto flex'>
                         <h3 className='center-text white-font'>This is the main body of the landing page </h3>
                     </div>
                 </div>
-                <div className="lower-body max-width max-height flex">
-                    <div className="auto-margin flex">
+                <div className="break auto">
+                    <div className="auto flex">
+                        <h5 className="center-text white-font auto">This is sample text</h5>
+                        <p className="center-text white-font auto"> This is a sample paragraph</p>
+                    </div>
+                </div>
+                <div className="lower-body max-width max-height flex ">
+                    <div className="auto flex">
                         <h3 className='center-text white-font'>This is the lower body of the landing Page</h3>
                     </div>
                 </div>
