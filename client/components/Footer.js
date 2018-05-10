13825d3b19948712a2ba54d66093eefd6e592e66
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-10 12:30:09 -0400

    feat: added footer to landing page

---
+++ client/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-container auto max-width flex">
+                <h5 className="white-font auto center-text">This is the Footer</h5>
+            </div>
+        )
+    }
+}
