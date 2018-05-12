f0154687574d6933cbd2e228c568076198f3c234
rutaitp <rk2906@nyu.edu>
2018-05-12 14:37:49 -0400

    style: add comments to sharedBroadcast

--- client/store/sharedBroadcast.js
+++ client/store/sharedBroadcast.js
@@ -1,7 +1,13 @@
+/**
+ * ACTION TYPES
+ */
 export const NEW_BROADCAST = 'NEW_BROADCAST';
 export const UPDATE_BROADCAST = 'UPDATE_BROADCAST';
 export const DELETE_BROADCAST = 'DELETE_BROADCAST';
 
+/**
+ * ACTION CREATORS
+ */
 export const createNewBroadcast = broadcast => ({
   type: NEW_BROADCAST,
   broadcast
