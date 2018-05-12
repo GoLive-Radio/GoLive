051f02d20748e2af7b7f7ba80cae61aafa98c9b5
rutaitp <rk2906@nyu.edu>
2018-05-12 13:59:01 -0400

    style: add missing semicolons

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
-import { Menu, Segment, Icon, Dropdown } from 'semantic-ui-react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {logout} from '../store';
+import { Menu, Segment, Dropdown } from 'semantic-ui-react';
 
 
 const Navbar = ({ handleClick, isLoggedIn}) => {
@@ -38,26 +38,26 @@ const Navbar = ({ handleClick, isLoggedIn}) => {
       )}
     </Segment>
   </div>
-)
-}
+);
+};
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout());
     }
-  }
-}
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
 
 /**
  * PROP TYPES
@@ -65,4 +65,4 @@ export default connect(mapState, mapDispatch)(Navbar)
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
